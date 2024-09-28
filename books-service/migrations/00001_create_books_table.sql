74357e7bfbc7ddfa10aaac2427c2a125b2056c81
Ali-Gorgani <ali.gorgani.fj@gmail.com>
2024-09-28 20:55:53 +0330

    structure

--- books-service/migrations/00001_create_books_table.sql
+++ books-service/migrations/00001_create_books_table.sql
@@ -5,7 +5,9 @@ CREATE TABLE books (
     title VARCHAR(255) NOT NULL,
     author VARCHAR(255) NOT NULL,
     published_year INT,
-    category VARCHAR(100)
+    category VARCHAR(100),
+    available BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 );
 -- +goose StatementEnd
 
